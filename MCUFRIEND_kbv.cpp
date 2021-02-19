d0d29c9111b13d8f2c661b8d8c7f9bb21af480ee
jbrightuniverse <30967260+jbrightuniverse@users.noreply.github.com>
2021-02-18 16:22:18 -0800

    remove things with no point

--- MCUFRIEND_kbv.cpp
+++ MCUFRIEND_kbv.cpp
@@ -1,7 +1,6 @@
 #define SUPPORT_7735
 
 #include "MCUFRIEND_kbv.h"
-
 #include "def_serial.h"
 
 #define MIPI_DCS_REV1   (1<<0)
@@ -159,49 +158,20 @@ int16_t MCUFRIEND_kbv::readGRAM(int16_t x, int16_t y, uint16_t * block, int16_t 
     uint16_t ret, dummy, _MR = _MW;
     int16_t n = w * h, row = 0, col = 0;
     uint8_t r, g, b, tmp;
-    if (!is8347 && _lcd_capable & MIPI_DCS_REV1) // HX8347 uses same register
-        _MR = 0x2E;
-    if (_lcd_ID == 0x1602) _MR = 0x2E;
+    _MR = 0x2E;
     setAddrWindow(x, y, x + w - 1, y + h - 1);
     while (n > 0) {
-        if (!(_lcd_capable & MIPI_DCS_REV1)) {
-            WriteCmdData(_MC, x + col);
-            WriteCmdData(_MP, y + row);
-        }
         CS_ACTIVE;
         WriteCmd(_MR);
         setReadDir();
-        if (_lcd_capable & READ_NODUMMY) {
-            ;
-        } else if ((_lcd_capable & MIPI_DCS_REV1) || _lcd_ID == 0x1289) {
-            READ_8(r);
-        } else {
-            READ_16(dummy);
-        }
-		if (_lcd_ID == 0x1511) READ_8(r);   //extra dummy for R61511
+        READ_8(r);
         while (n) {
-            if (_lcd_capable & READ_24BITS) {
-                READ_8(r);
-                READ_8(g);
-                READ_8(b);
-                if (_lcd_capable & READ_BGR)
-                    ret = color565(b, g, r);
-                else
-                    ret = color565(r, g, b);
-            } else {
-                READ_16(ret);
-                if (_lcd_capable & READ_LOWHIGH)
-                    ret = (ret >> 8) | (ret << 8);
-                if (_lcd_capable & READ_BGR)
-                    ret = (ret & 0x07E0) | (ret >> 11) | (ret << 11);
-            }
-#if defined(SUPPORT_9488_555)
-    if (is555) ret = color555_to_565(ret);
-#endif
+            READ_8(r);
+            READ_8(g);
+            READ_8(b);
+            ret = color565(r, g, b);
             *block++ = ret;
             n--;
-            if (!(_lcd_capable & AUTO_READINC))
-                break;
         }
         if (++col >= w) {
             col = 0;
@@ -212,8 +182,6 @@ int16_t MCUFRIEND_kbv::readGRAM(int16_t x, int16_t y, uint16_t * block, int16_t 
         CS_IDLE;
         setWriteDir();
     }
-    if (!(_lcd_capable & MIPI_DCS_REV1))
-        setAddrWindow(0, 0, width() - 1, height() - 1);
     return 0;
 }
 
@@ -238,143 +206,9 @@ void MCUFRIEND_kbv::setRotation(uint8_t r)
         val = 0xF8;             //MY=1, MX=1, MV=1, ML=1, BGR=1
         break;
     }
-    if (_lcd_capable & INVERT_GS)
-        val ^= 0x80;
-    if (_lcd_capable & INVERT_SS)
-        val ^= 0x40;
-    if (_lcd_capable & INVERT_RGB)
-        val ^= 0x08;
-    if (_lcd_capable & MIPI_DCS_REV1) {
-        if (_lcd_ID == 0x6814) {  //.kbv my weird 0x9486 might be 68140
-            GS = (val & 0x80) ? (1 << 6) : 0;   //MY
-            SS_v = (val & 0x40) ? (1 << 5) : 0;   //MX
-            val &= 0x28;        //keep MV, BGR, MY=0, MX=0, ML=0
-            d[0] = 0;
-            d[1] = GS | SS_v | 0x02;      //MY, MX
-            d[2] = 0x3B;
-            WriteCmdParamN(0xB6, 3, d);
-            goto common_MC;
-        } else if (_lcd_ID == 0x1963 || _lcd_ID == 0x9481 || _lcd_ID == 0x1511) {
-            if (val & 0x80)
-                val |= 0x01;    //GS
-            if ((val & 0x40))
-                val |= 0x02;    //SS
-            if (_lcd_ID == 0x1963) val &= ~0xC0;
-            if (_lcd_ID == 0x9481) val &= ~0xD0;
-            if (_lcd_ID == 0x1511) {
-                val &= ~0x10;   //remove ML
-                val |= 0xC0;    //force penguin 180 rotation
-            }
-//            val &= (_lcd_ID == 0x1963) ? ~0xC0 : ~0xD0; //MY=0, MX=0 with ML=0 for ILI9481
-            goto common_MC;
-        } else if (is8347) {
-            _MC = 0x02, _MP = 0x06, _MW = 0x22, _SC = 0x02, _EC = 0x04, _SP = 0x06, _EP = 0x08;
-            if (_lcd_ID == 0x0065) {             //HX8352-B
-                val |= 0x01;    //GS=1
-                if ((val & 0x10)) val ^= 0xD3;  //(ML) flip MY, MX, ML, SS, GS
-                if (r & 1) _MC = 0x82, _MP = 0x80;
-                else _MC = 0x80, _MP = 0x82;
-            }
-            if (_lcd_ID == 0x5252) {             //HX8352-A
-                val |= 0x02;   //VERT_SCROLLON
-                if ((val & 0x10)) val ^= 0xD4;  //(ML) flip MY, MX, SS. GS=1
-            }
-			goto common_BGR;
-        }
-      common_MC:
-        _MC = 0x2A, _MP = 0x2B, _MW = 0x2C, _SC = 0x2A, _EC = 0x2A, _SP = 0x2B, _EP = 0x2B;
-      common_BGR:
-        WriteCmdParamN(is8347 ? 0x16 : 0x36, 1, &val);
-        _lcd_madctl = val;
-//	    if (_lcd_ID	== 0x1963) WriteCmdParamN(0x13, 0, NULL);   //NORMAL mode
-    }
-    // cope with 9320 variants
-    else {
-        switch (_lcd_ID) {
-#if defined(SUPPORT_9225)
-        case 0x9225:
-            _SC = 0x37, _EC = 0x36, _SP = 0x39, _EP = 0x38;
-            _MC = 0x20, _MP = 0x21, _MW = 0x22;
-            GS = (val & 0x80) ? (1 << 9) : 0;
-            SS_v = (val & 0x40) ? (1 << 8) : 0;
-            WriteCmdData(0x01, GS | SS_v | 0x001C);       // set Driver Output Control
-            goto common_ORG;
-#endif
-#if defined(SUPPORT_0139) || defined(SUPPORT_0154)
-#ifdef SUPPORT_0139
-        case 0x0139:
-            _SC = 0x46, _EC = 0x46, _SP = 0x48, _EP = 0x47;
-            goto common_S6D;
-#endif
-#ifdef SUPPORT_0154
-        case 0x0154:
-            _SC = 0x37, _EC = 0x36, _SP = 0x39, _EP = 0x38;
-            goto common_S6D;
-#endif
-          common_S6D:
-            _MC = 0x20, _MP = 0x21, _MW = 0x22;
-            GS = (val & 0x80) ? (1 << 9) : 0;
-            SS_v = (val & 0x40) ? (1 << 8) : 0;
-            // S6D0139 requires NL = 0x27,  S6D0154 NL = 0x28
-            WriteCmdData(0x01, GS | SS_v | ((_lcd_ID == 0x0139) ? 0x27 : 0x28));
-            goto common_ORG;
-#endif
-        case 0x5420:
-        case 0x7793:
-        case 0x9326:
-		case 0xB509:
-            _MC = 0x200, _MP = 0x201, _MW = 0x202, _SC = 0x210, _EC = 0x211, _SP = 0x212, _EP = 0x213;
-            GS = (val & 0x80) ? (1 << 15) : 0;
-			uint16_t NL;
-			NL = ((432 / 8) - 1) << 9;
-            if (_lcd_ID == 0x9326 || _lcd_ID == 0x5420) NL >>= 1;
-            WriteCmdData(0x400, GS | NL);
-            goto common_SS;
-        default:
-            _MC = 0x20, _MP = 0x21, _MW = 0x22, _SC = 0x50, _EC = 0x51, _SP = 0x52, _EP = 0x53;
-            GS = (val & 0x80) ? (1 << 15) : 0;
-            WriteCmdData(0x60, GS | 0x2700);    // Gate Scan Line (0xA700)
-          common_SS:
-            SS_v = (val & 0x40) ? (1 << 8) : 0;
-            WriteCmdData(0x01, SS_v);     // set Driver Output Control
-          common_ORG:
-            ORG = (val & 0x20) ? (1 << 3) : 0;
-#ifdef SUPPORT_8230
-            if (_lcd_ID == 0x8230) {    // UC8230 has strange BGR and READ_BGR behaviour
-                if (rotation == 1 || rotation == 2) {
-                    val ^= 0x08;        // change BGR bit for LANDSCAPE and PORTRAIT_REV
-                }
-            }               
-#endif
-            if (val & 0x08)
-                ORG |= 0x1000;  //BGR
-            _lcd_madctl = ORG | 0x0030;
-            WriteCmdData(0x03, _lcd_madctl);    // set GRAM write direction and BGR=1.
-            break;
-#ifdef SUPPORT_1289
-        case 0x1289:
-            _MC = 0x4E, _MP = 0x4F, _MW = 0x22, _SC = 0x44, _EC = 0x44, _SP = 0x45, _EP = 0x46;
-            if (rotation & 1)
-                val ^= 0xD0;    // exchange Landscape modes
-            GS = (val & 0x80) ? (1 << 14) : 0;    //called TB (top-bottom), CAD=0
-            SS_v = (val & 0x40) ? (1 << 9) : 0;   //called RL (right-left)
-            ORG = (val & 0x20) ? (1 << 3) : 0;  //called AM
-            _lcd_drivOut = GS | SS_v | (REV << 13) | 0x013F;      //REV=0, BGR=0, MUX=319
-            if (val & 0x08)
-                _lcd_drivOut |= 0x0800; //BGR
-            WriteCmdData(0x01, _lcd_drivOut);   // set Driver Output Control
-            if (is9797) WriteCmdData(0x11, ORG | 0x4C30); else  // DFM=2, DEN=1, WM=1, TY=0
-            WriteCmdData(0x11, ORG | 0x6070);   // DFM=3, EN=0, TY=1
-            break;
-#endif
-		}
-    }
-    if ((rotation & 1) && ((_lcd_capable & MV_AXIS) == 0)) {
-        uint16_t x;
-        x = _MC, _MC = _MP, _MP = x;
-        x = _SC, _SC = _SP, _SP = x;    //.kbv check 0139
-        x = _EC, _EC = _EP, _EP = x;    //.kbv check 0139
-    }
+    _MC = 0x2A, _MP = 0x2B, _MW = 0x2C, _SC = 0x2A, _EC = 0x2A, _SP = 0x2B, _EP = 0x2B;
+    WriteCmdParamN(is8347 ? 0x16 : 0x36, 1, &val);
+    _lcd_madctl = val;
     setAddrWindow(0, 0, width() - 1, height() - 1);
     vertScroll(0, HEIGHT, 0);   //reset scrolling after a rotation
 }
@@ -384,64 +218,19 @@ void MCUFRIEND_kbv::drawPixel(int16_t x, int16_t y, uint16_t color)
     // MCUFRIEND just plots at edge if you try to write outside of the box:
     if (x < 0 || y < 0 || x >= width() || y >= height())
         return;
-#if defined(SUPPORT_9488_555)
-    if (is555) color = color565_to_555(color);
-#endif
     setAddrWindow(x, y, x, y);
-//    CS_ACTIVE; WriteCmd(_MW); write16(color); CS_IDLE; //-0.01s +98B
-    if (is9797) { CS_ACTIVE; WriteCmd(_MW); write24(color); CS_IDLE;} else
     WriteCmdData(_MW, color);
 }
 
 void MCUFRIEND_kbv::setAddrWindow(int16_t x, int16_t y, int16_t x1, int16_t y1)
 {
-#if defined(OFFSET_9327)
-	if (_lcd_ID == 0x9327) {
-	    if (rotation == 2) y += OFFSET_9327, y1 += OFFSET_9327;
-	    if (rotation == 3) x += OFFSET_9327, x1 += OFFSET_9327;
-    }
-#endif
-#if 1
-    if (_lcd_ID == 0x1526 && (rotation & 1)) {
-		int16_t dx = x1 - x, dy = y1 - y;
-		if (dy == 0) { y1++; }
-		else if (dx == 0) { x1 += dy; y1 -= dy; }
-    }
-#endif
-    if (_lcd_capable & MIPI_DCS_REV1) {
-        WriteCmdParam4(_SC, x >> 8, x, x1 >> 8, x1);   //Start column instead of _MC
-        WriteCmdParam4(_SP, y >> 8, y, y1 >> 8, y1);   //
-        if (is8347 && _lcd_ID == 0x0065) {             //HX8352-B has separate _MC, _SC
-            uint8_t d[2];
-            d[0] = x >> 8; d[1] = x;
-            WriteCmdParamN(_MC, 2, d);                 //allows !MV_AXIS to work
-            d[0] = y >> 8; d[1] = y;
-            WriteCmdParamN(_MP, 2, d);
-        }
-    } else {
-        WriteCmdData(_MC, x);
-        WriteCmdData(_MP, y);
-        if (!(x == x1 && y == y1)) {  //only need MC,MP for drawPixel
-            if (_lcd_capable & XSA_XEA_16BIT) {
-                if (rotation & 1)
-                    y1 = y = (y1 << 8) | y;
-                else
-                    x1 = x = (x1 << 8) | x;
-            }
-            WriteCmdData(_SC, x);
-            WriteCmdData(_SP, y);
-            WriteCmdData(_EC, x1);
-            WriteCmdData(_EP, y1);
-        }
-    }
+    WriteCmdParam4(_SC, x >> 8, x, x1 >> 8, x1);   //Start column instead of _MC
+    WriteCmdParam4(_SP, y >> 8, y, y1 >> 8, y1);   //
 }
 
 void MCUFRIEND_kbv::fillRect(int16_t x, int16_t y, int16_t w, int16_t h, uint16_t color)
 {
     int16_t end;
-#if defined(SUPPORT_9488_555)
-    if (is555) color = color565_to_555(color);
-#endif
     if (w < 0) {
         w = -w;
         x -= w;
@@ -473,55 +262,12 @@ void MCUFRIEND_kbv::fillRect(int16_t x, int16_t y, int16_t w, int16_t h, uint16_
     uint8_t hi = color >> 8, lo = color & 0xFF;
     while (h-- > 0) {
         end = w;
-#if USING_16BIT_BUS
-#if defined(__MK66FX1M0__)      //180MHz M4
-#define STROBE_16BIT {WR_ACTIVE4;WR_ACTIVE;WR_IDLE4;WR_IDLE;}   //56ns
-#elif defined(__SAM3X8E__)      //84MHz M3
-#define STROBE_16BIT {WR_ACTIVE4;WR_ACTIVE2;WR_IDLE4;WR_IDLE2;} //286ns ?ILI9486
-//#define STROBE_16BIT {WR_ACTIVE4;WR_ACTIVE;WR_IDLE4;WR_IDLE;} //238ns SSD1289
-//#define STROBE_16BIT {WR_ACTIVE2;WR_ACTIVE;WR_IDLE2;}      //119ns RM68140
-#else                           //16MHz AVR
-#define STROBE_16BIT {WR_ACTIVE;WR_ACTIVE;WR_IDLE; }            //375ns ?ILI9486
-#endif
-        write_16(color);        //we could just do the strobe
-        lo = end & 7;
-        hi = end >> 3;
-        if (hi)
-            do {
-                STROBE_16BIT;
-                STROBE_16BIT;
-                STROBE_16BIT;
-                STROBE_16BIT;
-                STROBE_16BIT;
-                STROBE_16BIT;
-                STROBE_16BIT;
-                STROBE_16BIT;
-            } while (--hi > 0);
-        while (lo-- > 0) {
-            STROBE_16BIT;
-        }
-#else
-#if defined(SUPPORT_1289)
-        if (is9797) {
-             uint8_t r = color565_to_r(color);
-             uint8_t g = color565_to_g(color);
-             uint8_t b = color565_to_b(color);
-             do {
-                 write8(r);
-                 write8(g);
-                 write8(b);
-             } while (--end != 0);
-        } else
-#endif
         do {
             write8(hi);
             write8(lo);
         } while (--end != 0);
-#endif
     }
     CS_IDLE;
-    if (!(_lcd_capable & MIPI_DCS_REV1) || ((_lcd_ID == 0x1526) && (rotation & 1)))
-        setAddrWindow(0, 0, width() - 1, height() - 1);
 }
 
 static void pushColors_any(uint16_t cmd, uint8_t * block, int16_t n, bool first, uint8_t flags)
@@ -552,10 +298,6 @@ static void pushColors_any(uint16_t cmd, uint8_t * block, int16_t n, bool first,
             l = (*block++);
 		}
         color = (isbigend) ? (h << 8 | l) :  (l << 8 | h);
-#if defined(SUPPORT_9488_555)
-        if (is555) color = color565_to_555(color);
-#endif
-        if (is9797) write24(color); else
         write16(color);
     }
     CS_IDLE;
@@ -576,138 +318,32 @@ void MCUFRIEND_kbv::pushColors(const uint8_t * block, int16_t n, bool first, boo
 
 void MCUFRIEND_kbv::vertScroll(int16_t top, int16_t scrollines, int16_t offset)
 {
-#if defined(OFFSET_9327)
-	if (_lcd_ID == 0x9327) {
-	    if (rotation == 2 || rotation == 3) top += OFFSET_9327;
-    }
-#endif
     int16_t bfa = HEIGHT - top - scrollines;  // bottom fixed area
     int16_t vsp;
     int16_t sea = top;
-	if (_lcd_ID == 0x9327) bfa += 32;
     if (offset <= -scrollines || offset >= scrollines) offset = 0; //valid scroll
 	vsp = top + offset; // vertical start position
     if (offset < 0)
         vsp += scrollines;          //keep in unsigned range
     sea = top + scrollines - 1;
-    if (_lcd_capable & MIPI_DCS_REV1) {
-        uint8_t d[6];           // for multi-byte parameters
-/*
-        if (_lcd_ID == 0x9327) {        //panel is wired for 240x432 
-            if (rotation == 2 || rotation == 3) { //180 or 270 degrees
-                if (scrollines == HEIGHT) {
-                    scrollines = 432;   // we get a glitch but hey-ho
-                    vsp -= 432 - HEIGHT;
-                }
-                if (vsp < 0)
-                    vsp += 432;
-            }
-            bfa = 432 - top - scrollines;
-        }
-*/
-        d[0] = top >> 8;        //TFA
-        d[1] = top;
-        d[2] = scrollines >> 8; //VSA
-        d[3] = scrollines;
-        d[4] = bfa >> 8;        //BFA
-        d[5] = bfa;
-        WriteCmdParamN(is8347 ? 0x0E : 0x33, 6, d);
-//        if (offset == 0 && rotation > 1) vsp = top + scrollines;   //make non-valid
-		d[0] = vsp >> 8;        //VSP
-        d[1] = vsp;
-        WriteCmdParamN(is8347 ? 0x14 : 0x37, 2, d);
-		if (is8347) { 
-		    d[0] = (offset != 0) ? (_lcd_ID == 0x8347 ? 0x02 : 0x08) : 0;
-			WriteCmdParamN(_lcd_ID == 0x8347 ? 0x18 : 0x01, 1, d);  //HX8347-D
-		} else if (offset == 0 && (_lcd_capable & MIPI_DCS_REV1)) {
-			WriteCmdParamN(0x13, 0, NULL);    //NORMAL i.e. disable scroll
-		}
-		return;
-    }
-    // cope with 9320 style variants:
-    switch (_lcd_ID) {
-    case 0x7783:
-        WriteCmdData(0x61, _lcd_rev);   //!NDL, !VLE, REV
-        WriteCmdData(0x6A, vsp);        //VL#
-        break;
-#ifdef SUPPORT_0139 
-    case 0x0139:
-        WriteCmdData(0x07, 0x0213 | (_lcd_rev << 2));  //VLE1=1, GON=1, REV=x, D=3
-        WriteCmdData(0x41, vsp);  //VL# check vsp
-        break;
-#endif
-#if defined(SUPPORT_0154) || defined(SUPPORT_9225)  //thanks tongbajiel
-    case 0x9225:
-	case 0x0154:
-        WriteCmdData(0x31, sea);        //SEA
-        WriteCmdData(0x32, top);        //SSA
-        WriteCmdData(0x33, vsp - top);  //SST
-        break;
-#endif
-#ifdef SUPPORT_1289
-    case 0x1289:
-        WriteCmdData(0x41, vsp);        //VL#
-        break;
-#endif
-	case 0x5420:
-    case 0x7793:
-	case 0x9326:
-	case 0xB509:
-        WriteCmdData(0x401, (1 << 1) | _lcd_rev);       //VLE, REV 
-        WriteCmdData(0x404, vsp);       //VL# 
-        break;
-    default:
-        // 0x6809, 0x9320, 0x9325, 0x9335, 0xB505 can only scroll whole screen
-        WriteCmdData(0x61, (1 << 1) | _lcd_rev);        //!NDL, VLE, REV
-        WriteCmdData(0x6A, vsp);        //VL#
-        break;
-    }
+    uint8_t d[6];           // for multi-byte parameters
+    d[0] = top >> 8;        //TFA
+    d[1] = top;
+    d[2] = scrollines >> 8; //VSA
+    d[3] = scrollines;
+    d[4] = bfa >> 8;        //BFA
+    d[5] = bfa;
+    WriteCmdParamN(0x33, 6, d);
+	d[0] = vsp >> 8;        //VSP
+	d[1] = vsp;
+    WriteCmdParamN(0x37, 2, d);
+    if (offset == 0) 
+	WriteCmdParamN(0x13, 0, NULL);    //NORMAL i.e. disable scroll
 }
 
 void MCUFRIEND_kbv::invertDisplay(bool i)
 {
-    uint8_t val;
-    _lcd_rev = ((_lcd_capable & REV_SCREEN) != 0) ^ i;
-    if (_lcd_capable & MIPI_DCS_REV1) {
-        if (is8347) {
-            // HX8347D: 0x36 Panel Characteristic. REV_Panel
-            // HX8347A: 0x36 is Display Control 10
-            if (_lcd_ID == 0x8347 || _lcd_ID == 0x5252) // HX8347-A, HX5352-A
-			    val = _lcd_rev ? 6 : 2;       //INVON id bit#2,  NORON=bit#1
-            else val = _lcd_rev ? 8 : 10;     //HX8347-D, G, I: SCROLLON=bit3, INVON=bit1
-            // HX8347: 0x01 Display Mode has diff bit mapping for A, D 
-            WriteCmdParamN(0x01, 1, &val);
-        } else
-            WriteCmdParamN(_lcd_rev ? 0x21 : 0x20, 0, NULL);
-        return;
-    }
-    // cope with 9320 style variants:
-    switch (_lcd_ID) {
-#ifdef SUPPORT_0139
-    case 0x0139:
-#endif
-    case 0x9225:                                        //REV is in reg(0x07) like Samsung
-    case 0x0154:
-        WriteCmdData(0x07, 0x13 | (_lcd_rev << 2));     //.kbv kludge
-        break;
-#ifdef SUPPORT_1289
-    case 0x1289:
-        _lcd_drivOut &= ~(1 << 13);
-        if (_lcd_rev)
-            _lcd_drivOut |= (1 << 13);
-        WriteCmdData(0x01, _lcd_drivOut);
-        break;
-#endif
-	case 0x5420:
-    case 0x7793:
-    case 0x9326:
-	case 0xB509:
-        WriteCmdData(0x401, (1 << 1) | _lcd_rev);       //.kbv kludge VLE 
-        break;
-    default:
-        WriteCmdData(0x61, _lcd_rev);
-        break;
-    }
+    WriteCmdParamN(i ? 0x21 : 0x20, 0, NULL);
 }
 
 #define TFTLCD_DELAY 0xFFFF
